11cddfed83f3e355dd9ae1eca0f1c3b1e837844b
Sylvester Elbo Jensen <Sylvesterelbo@gmail.com>
2026-04-20 13:24:15 +0200

    Input&Output

--- Program.cs
+++ Program.cs
@@ -1,1 +1,31 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert Your Number: ");
+            var tal = Console.ReadLine();
+            Console.WriteLine("Your number is" + tal);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
