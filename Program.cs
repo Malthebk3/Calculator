60ecc9b70c27ea6ed55d545a7b4d06b7925fcf6c
Sylvester Elbo Jensen <Sylvesterelbo@gmail.com>
2026-04-20 13:25:43 +0200

    Input&Output

--- Program.cs
+++ Program.cs
@@ -1,31 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Calculator
+namespace Csharp_Programming_Examples
 {
-    class Program
+    internal class calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Insert Your Number: ");
-            var tal = Console.ReadLine();
-            Console.WriteLine("Your number is" + tal);
+            int tal1, tal2,choice;
+            float c;
+            Console.WriteLine("==========Calculator==========");
+            Console.WriteLine("\n Enter your first Number");
+            tal1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n Enter your second Number");
+            tal2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please select any below");
+            Console.WriteLine("1. Addition");
+            Console.WriteLine("2. Subtraction");
+            Console.WriteLine("3. Multiply");
+            Console.WriteLine("4. Divided");
+            choice = Convert.ToInt32(Console.ReadLine());
+
         }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     }
 
 }
+
